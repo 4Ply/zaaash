2042fc9b88611917e69e8a9c336ddec16039156e
4Ply <gamer.4ply@gmail.com>
2026-03-27 14:06:36 -0700

    Add missing space to if statement

--- setup.sh
+++ setup.sh
@@ -25,7 +25,7 @@ if ! require brew; then
   echo "Installing Hombrew"
   export NONINTERACTIVE=1
   if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    if [ -d "/home/linuxbrew/.linuxbrew/bin/brew"]; then
+    if [ -d "/home/linuxbrew/.linuxbrew/bin/brew" ]; then
       alias brew='sudo -Hu linuxbrew brew'
     else
       /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
